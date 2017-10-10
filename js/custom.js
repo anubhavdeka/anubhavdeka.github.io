46509253678fb37871e68bfa6b97b75caf38285b
Anubhav Deka <adeka14@gmail.com>
2017-10-10 20:35:42 +0530

    cosmetics

--- js/custom.js
+++
@@ -1,9 +0,0 @@
-/* when a user clicks, toggle the 'is-animating' class */
-$(".heart").on('click touchstart', function(){
-  $(this).toggleClass('is_animating');
-});
-
-/*when the animation is over, remove the class*/
-$(".heart").on('animationend', function(){
-  $(this).toggleClass('is_animating');
-});
